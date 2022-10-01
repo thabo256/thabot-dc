98f87435244811d0d433855784aeec088d85e991
thabo256 <severin@c-moon.de>
2022-10-01 16:33:07 +0200

    added /nick

---
+++ commands/nick.js
@@ -0,0 +1,17 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('nick')
+    .setDescription('nick other members')
+    .addStringOption((option) => option.setName('name').setDescription('new nickname)').setRequired(true))
+    .addUserOption((option) => option.setName('user').setDescription('user to nick')),
+  test: true,
+  async execute(interaction) {
+    const name = interaction.options.getString('name');
+    const user = interaction.options.getUser('user');
+    const member = user ? interaction.guild.members.cache.get(user.id) : interaction.member;
+    await member.setNickname(name)
+    await interaction.reply(`set nickname of \`${member.user.tag}\` to \`${name}\``);
+  },
+};
