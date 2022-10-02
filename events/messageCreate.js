6815f908beb6dadb98d4733caf56e6d5dca0cb6f
thabo256 <severin@c-moon.de>
2022-10-02 22:23:16 +0200

    added gute nacht reply

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,11 +1,14 @@
 module.exports = {
   name: 'messageCreate',
   async execute(message) {
-    if (message.content === 'ping') {
+    const m = message.content.toLowerCase();
+    if (m === 'ping') {
       await message.react('🇵');
       await message.react('🇮');
       await message.react('🇳');
       await message.react('🇬');
+    } else if (m.includes('gute nacht')) {
+      await message.channel.send('┏━━┳┓┏┳━━┳━┓┏━┳┳━━┳━┳┓┏┳━━┓\n┃┏━┫┃┃┣┓┏┫╺┫┃┃┃┃╺╸┃┏┫┗┛┣┓┏┛\n┃┗╸┃┗┛┃┃┃┃╺┫┃┃┃┃┏┓┃┗┫┏┓┃┃┃\n┗━━┻━━┛┗┛┗━┛┗┻━┻┛┗┻━┻┛┗┛┗┛');
     }
   },
 };
