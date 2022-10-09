d814bbf7dada7b94fefdf7248e207d2023479847
thabo256 <severin@c-moon.de>
2022-10-09 11:27:31 +0200

    removed jia role

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,9 +1,9 @@
 module.exports = {
   name: 'guildMemberAdd',
   async execute(member) {
-    if (member.guild.id === '1025777907790008361') {
-      const newRole = member.guild.roles.cache.find((role) => role.name === 'JIA10');
-      member.roles.add(newRole);
-    }
+    // if (member.guild.id === 'ID') {
+    //   const newRole = member.guild.roles.cache.find((role) => role.name === 'ROLE');
+    //   member.roles.add(newRole);
+    // }
   },
 };
