9da219dd90ed1bafa34cbda7340ed4532b7e476f
thabo <severin@c-moon.de>
2025-02-15 17:21:15 +0100

    update command handling and move to event file

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,4 +1,37 @@
+const { Events, MessageFlags } = require('discord.js');
+
 module.exports = {
-  name: 'interactionCreate',
-  async execute(interaction) {},
+  name: Events.InteractionCreate,
+  async execute(interaction) {
+    if (interaction.isChatInputCommand()) {
+      // Dynamically executing commands
+      const command = interaction.client.commands.get(interaction.commandName);
+
+      if (!command) return;
+
+      try {
+        await command.execute(interaction);
+      } catch (error) {
+        console.error(error);
+        if (interaction.replied || interaction.deferred) {
+          await interaction.followUp({ content: 'There was an error while executing this command!', flags: MessageFlags.Ephemeral }).catch(console.error);
+        } else {
+          await interaction.reply({ content: 'There was an error while executing this command!', flags: MessageFlags.Ephemeral }).catch(console.error);
+        }
+      }
+    } else if (interaction.isButton()) {
+      // Dynamically executing buttons
+      const ids = interaction.customId.split('-');
+      const button = interaction.client.buttons.get(ids[0]);
+
+      if (!button) return;
+
+      try {
+        await button.execute(interaction, ids);
+      } catch (error) {
+        console.error(error);
+        await interaction.reply({ content: 'There was an error while handling this button press!', flags: MessageFlags.Ephemeral }).catch(console.error);
+      }
+    }
+  },
 };
--- index.js
+++ index.js
@@ -3,24 +3,10 @@ const fs = require('node:fs');
 const path = require('node:path');
 
 // Require the necessary discord.js classes
-const { Client, GatewayIntentBits, Collection } = require('discord.js');
+const { Client, GatewayIntentBits, Collection, Events } = require('discord.js');
 const client = new Client({
   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMembers, /* GatewayIntentBits.GuildPresences,  */ GatewayIntentBits.MessageContent],
 });
-
-// dynamically retrieve event files
-const eventsPath = path.join(__dirname, 'events');
-const eventFiles = fs.readdirSync(eventsPath).filter((file) => file.endsWith('.js'));
-
-for (const file of eventFiles) {
-  const filePath = path.join(eventsPath, file);
-  const event = require(filePath);
-  if (event.once) {
-    client.once(event.name, (...args) => event.execute(...args));
-  } else {
-    client.on(event.name, (...args) => event.execute(...args));
-  }
-}
 
 // dynamically retrieve command files
 client.commands = new Collection();
@@ -44,38 +30,19 @@ for (const file of buttonFiles) {
   client.buttons.set(button.name, button);
 }
 
-client.on('interactionCreate', async (interaction) => {
-  if (interaction.isChatInputCommand()) {
-    // Dynamically executing commands
-    const command = client.commands.get(interaction.commandName);
+// dynamically retrieve event files
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter((file) => file.endsWith('.js'));
 
-    if (!command) return;
-
-    try {
-      await command.execute(interaction);
-    } catch (error) {
-      console.error(error);
-      await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true }).catch(console.error);
-    }
-  } else if (interaction.isButton()) {
-    // Dynamically executing buttons
-    const ids = interaction.customId.split('-');
-    const button = client.buttons.get(ids[0]);
-
-    if (!button) return;
-
-    try {
-      await button.execute(interaction, ids);
-    } catch (error) {
-      console.error(error);
-      await interaction.reply({ content: 'There was an error while handling this button press!', ephemeral: true });
-    }
+for (const file of eventFiles) {
+  const filePath = path.join(eventsPath, file);
+  const event = require(filePath);
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args));
   }
-});
-
-// client.on('guildMemberAdd', member => {
-//   console.log('member');
-// })
+}
 
 // login using login token
 client.login(process.env.LOGIN_TOKEN);
