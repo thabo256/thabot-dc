4ab8d2b6ec7b5846facc0fe884d8a7849c2b0ada
thabo <severin@c-moon.de>
2025-02-16 13:14:57 +0100

    add required imports

--- commands/spam.js
+++ commands/spam.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, MessageFlags } = require('discord.js');
+const { SlashCommandBuilder, MessageFlags, InteractionContextType } = require('discord.js');
 
 module.exports = {
   data: new SlashCommandBuilder()
