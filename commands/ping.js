28919c97b690327873c47db99669cd4bc65c2574
thabo <severin@c-moon.de>
2025-02-16 15:09:23 +0100

    make ping command actually show latency

--- commands/ping.js
+++ commands/ping.js
@@ -1,8 +1,14 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, MessageFlags, InteractionContextType, ApplicationIntegrationType } = require('discord.js');
 
 module.exports = {
-  data: new SlashCommandBuilder().setName('ping').setDescription('Replies with Pong!'),
+  data: new SlashCommandBuilder()
+    .setName('ping')
+    .setDescription("Displays the bot's latency.")
+    .setContexts([InteractionContextType.Guild, InteractionContextType.BotDM, InteractionContextType.PrivateChannel])
+    .setIntegrationTypes([ApplicationIntegrationType.GuildInstall, ApplicationIntegrationType.UserInstall]),
   async execute(interaction) {
-    return interaction.reply('Pong!');
+    await interaction.reply({ content: 'Pinging...', flags: MessageFlags.Ephemeral });
+    const sent = await interaction.editReply('Pinging....');
+    interaction.editReply(`Roundtrip latency: ${sent.createdTimestamp - interaction.createdTimestamp}ms`);
   },
 };
