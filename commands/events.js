075c0b2b1ece46d53fcf2138f72dfa3243f9858d
thabo <severin@c-moon.de>
2025-02-20 21:45:51 +0100

    add /events command

---
+++ commands/events.js
@@ -0,0 +1,28 @@
+const { SlashCommandBuilder, InteractionContextType, ApplicationIntegrationType, MessageFlags, GuildScheduledEventManager, GuildScheduledEventCreateOptions } = require('discord.js');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('events')
+    .setDescription('show all events in the server')
+    .setContexts(InteractionContextType.Guild)
+    .setIntegrationTypes(ApplicationIntegrationType.GuildInstall),
+  async execute(interaction) {
+    const eventManager = new GuildScheduledEventManager(interaction.guild);
+
+    const events = await eventManager.fetch();
+
+    console.log(events);
+    
+
+    // const event = await eventManager.create({
+    //   name: 'Test Event',
+    //   scheduledStartTime: new Date(Date.now() + 1000 * 60 * 60),
+    //   scheduledEndTime: new Date(Date.now() + 1000 * 60 * 60 * 2),
+    //   privacyLevel: 2, // Guild only
+    //   entityType: 3, // External
+    //   entityMetadata: { location: 'Berlin' },
+    // });
+
+    await interaction.reply({ content: `aaa`, flags: MessageFlags.Ephemeral });
+  },
+};
