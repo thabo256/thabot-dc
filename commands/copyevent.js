efce48cd92e33c432f532b6812ab72d5156ba209
thabo <severin@c-moon.de>
2025-02-27 20:59:14 +0100

    fix copyevent command

--- commands/copyevent.js
+++ commands/copyevent.js
@@ -17,26 +17,33 @@ module.exports = {
 
     for (const guild of guilds.values()) {
       const manager = new GuildScheduledEventManager(guild);
-      console.log(manager);
-      
-      event = await manager.fetch(id);
-      if (event) {
-        break;
-      }
+
+      try {
+        event = await manager.fetch(id);
+        if (event) {
+          break;
+        }
+      } catch (e) {}
     }
 
-    console.log(event);
-    
+    if (!event) {
+      await interaction.reply({ content: 'Event not found\n-# make shure the id is correct and the bot has the permissions to see the event', flags: MessageFlags.Ephemeral });
+      return;
+    }
 
-    // const event = await eventManager.create({
-    //   name: 'Test Event',
-    //   scheduledStartTime: new Date(Date.now() + 1000 * 60 * 60),
-    //   scheduledEndTime: new Date(Date.now() + 1000 * 60 * 60 * 2),
-    //   privacyLevel: 2, // Guild only
-    //   entityType: 3, // External
-    //   entityMetadata: { location: 'Berlin' },
-    // });
+    const location = event.entityType === 3 ? event.entityMetadata.location : '';
 
-    await interaction.reply('No events scheduled');
+    await eventManager.create({
+      name: event.name,
+      description: event.description,
+      channel: interaction.channelId,
+      scheduledStartTime: event.scheduledStartTimestamp,
+      scheduledEndTime: event.scheduledEndTimestamp ?? event.scheduledStartTimestamp + 1000 * 60 * 60,
+      privacyLevel: 2, // Guild only
+      entityType: 3, // External
+      entityMetadata: { location },
+    });
+
+    await interaction.reply('Event copied successfully');
   },
 };
