afa4cfd2e4f73da7791e88d23a32cdb1fc530577
thabo <severin@c-moon.de>
2025-03-01 19:49:11 +0100

    add generation of backup file

--- commands/backup.js
+++ commands/backup.js
@@ -2,27 +2,50 @@ const fs = require('node:fs');
 const path = require('node:path');
 require('dotenv').config();
 
-const { SlashCommandBuilder, MessageFlags, InteractionContextType, ApplicationIntegrationType } = require('discord.js');
+const { SlashCommandBuilder, MessageFlags, InteractionContextType, ApplicationIntegrationType, AttachmentBuilder } = require('discord.js');
+
+const fetchChannel = async (channel) => {
+  return { id: channel.id, name: channel.name, viewable: channel.viewable };
+};
 
 module.exports = {
-  data: new SlashCommandBuilder()
-    .setName('backup')
-    .setDescription('backup this server')
-    .setContexts([InteractionContextType.Guild])
-    .setIntegrationTypes([ApplicationIntegrationType.GuildInstall]),
-    test: true,
+  data: new SlashCommandBuilder().setName('backup').setDescription('backup this server').setContexts([InteractionContextType.Guild]).setIntegrationTypes([ApplicationIntegrationType.GuildInstall]),
+  test: true,
   async execute(interaction) {
     // check for permission
     if (interaction.user.id !== process.env.DEVELOPER_ID) {
       return interaction.reply({ content: 'You do not have permission to use this command.', flags: MessageFlags.Ephemeral });
     }
 
+    await interaction.reply('Creating backup...');
+
     const channels = await interaction.guild.channels.fetch();
+    const backup = { id: interaction.guild.id, name: interaction.guild.name, channels: [{ textChannels: [], voiceChannels: [] }] };
 
     for (const channel of channels.values()) {
-      console.log(channel.parent?.name, channel.name, channel.isTextBased(), channel.viewable, channel.position);
+      if (!channel.isTextBased()) continue;
+
+      if (channel.parent == undefined) {
+        if (channel.isVoiceBased()) {
+          backup.channels[0].voiceChannels[channel.position] = await fetchChannel(channel);
+        } else {
+          backup.channels[0].textChannels[channel.position] = await fetchChannel(channel);
+        }
+      } else {
+        if (backup.channels[channel.parent.position + 1] == undefined) {
+          backup.channels[channel.parent.position + 1] = { id: channel.parent.id, name: channel.parent.name, textChannels: [], voiceChannels: [] };
+        }
+
+        if (channel.isVoiceBased()) {
+          backup.channels[channel.parent.position + 1].voiceChannels[channel.position] = await fetchChannel(channel);
+        } else {
+          backup.channels[channel.parent.position + 1].textChannels[channel.position] = await fetchChannel(channel);
+        }
+      }
     }
-    
-    interaction.reply('Backup failed');
+
+    const file = new AttachmentBuilder(Buffer.from(JSON.stringify(backup, null, 2)), { name: 'backup.json' });
+
+    return interaction.editReply({ content: 'download backup file:', files: [file] });
   },
 };
