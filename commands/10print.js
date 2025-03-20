e9447a51b5589e3877b5bd22904951d1bbb68a8f
thabo <severin@c-moon.de>
2025-03-20 23:44:39 +0100

    add more options

--- commands/10print.js
+++ commands/10print.js
@@ -6,15 +6,21 @@ module.exports = {
     .setDescription('10print')
     .addNumberOption((option) => option.setName('width').setDescription('width of the output').setRequired(true).setMinValue(10).setMaxValue(100))
     .addNumberOption((option) => option.setName('height').setDescription('height of the output').setRequired(true).setMinValue(10).setMaxValue(19))
+    .addBooleanOption((option) => option.setName('include-x').setDescription('include the ╳'))
     .setContexts([InteractionContextType.Guild, InteractionContextType.BotDM, InteractionContextType.PrivateChannel])
     .setIntegrationTypes([ApplicationIntegrationType.GuildInstall, ApplicationIntegrationType.UserInstall]),
   async execute(interaction) {
     const width = interaction.options.getNumber('width');
     const height = interaction.options.getNumber('height');
+    const includeX = interaction.options.getBoolean('include-x');
 
     await interaction.reply('...');
 
     const chars = ['╱', '╲'];
+
+    if (includeX) {
+      chars.push('╳');
+    }
 
     let output = '';
 
