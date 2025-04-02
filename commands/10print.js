1fde6bd72f8ecd121a212ef587d1dcef8974d28f
thabo <severin@c-moon.de>
2025-04-02 22:06:36 +0200

    add support for custom chars

--- commands/10print.js
+++ commands/10print.js
@@ -7,18 +7,20 @@ module.exports = {
     .addNumberOption((option) => option.setName('width').setDescription('width of the output').setRequired(true).setMinValue(10).setMaxValue(100))
     .addNumberOption((option) => option.setName('height').setDescription('height of the output').setRequired(true).setMinValue(10).setMaxValue(19))
     .addBooleanOption((option) => option.setName('include-x').setDescription('include the ╳'))
+    .addStringOption((option) => option.setName('custom-chars').setDescription('custom characters to use'))
     .setContexts([InteractionContextType.Guild, InteractionContextType.BotDM, InteractionContextType.PrivateChannel])
     .setIntegrationTypes([ApplicationIntegrationType.GuildInstall, ApplicationIntegrationType.UserInstall]),
   async execute(interaction) {
     const width = interaction.options.getNumber('width');
     const height = interaction.options.getNumber('height');
     const includeX = interaction.options.getBoolean('include-x');
+    const customChars = interaction.options.getString('custom-chars');
 
     await interaction.reply('...');
 
-    const chars = ['╱', '╲'];
+    const chars = customChars ? customChars.split('') : ['╱', '╲'];
 
-    if (includeX) {
+    if (includeX && !customChars) {
       chars.push('╳');
     }
 
