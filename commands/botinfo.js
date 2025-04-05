28d5efb10d7d4a3d8825c8c0a16f7471c5764b03
thabo <severin@c-moon.de>
2025-04-06 00:14:36 +0200

    add installed users info

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -6,13 +6,18 @@ module.exports = {
     .setDescription('get ')
     .setContexts([InteractionContextType.Guild, InteractionContextType.BotDM, InteractionContextType.PrivateChannel])
     .setIntegrationTypes([ApplicationIntegrationType.GuildInstall, ApplicationIntegrationType.UserInstall])
-    .addSubcommand((subcommand) => subcommand.setName('servers').setDescription('see the servers the bot is in')),
+    .addSubcommand((subcommand) => subcommand.setName('servers').setDescription('see the servers the bot is in'))
+    .addSubcommand((subcommand) => subcommand.setName('users').setDescription('see the users that installed the bot')),
   test: true,
   async execute(interaction) {
     if (interaction.options.getSubcommand() === 'servers') {
       const guilds = await interaction.client.guilds.fetch();
       const response = guilds.map((guild) => `**${guild.name}** (\`${guild.id}\`)`).join('\n');
-      await interaction.reply({ content: `The bot is in the following servers:\n${response}`, flags: MessageFlags.Ephemeral });
+      return interaction.reply({ content: `The bot is in the following servers:\n${response}`, flags: MessageFlags.Ephemeral });
+    }
+    if (interaction.options.getSubcommand() === 'users') {
+      const app = await interaction.client.application.fetch();
+      return interaction.reply({ content: `The bot is installed by approximately \`${app.approximateUserInstallCount}\` users`, flags: MessageFlags.Ephemeral });
     }
   },
 };
