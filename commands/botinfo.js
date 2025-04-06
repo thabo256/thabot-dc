2cb287c2844bcbb07658a8a447becb824d8440c2
thabo <severin@c-moon.de>
2025-04-06 15:00:53 +0200

    add uptime info

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, MessageFlags, InteractionContextType, ApplicationIntegrationType } = require('discord.js');
+const { SlashCommandBuilder, MessageFlags, InteractionContextType, ApplicationIntegrationType, time } = require('discord.js');
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -7,7 +7,8 @@ module.exports = {
     .setContexts([InteractionContextType.Guild, InteractionContextType.BotDM, InteractionContextType.PrivateChannel])
     .setIntegrationTypes([ApplicationIntegrationType.GuildInstall, ApplicationIntegrationType.UserInstall])
     .addSubcommand((subcommand) => subcommand.setName('servers').setDescription('see the servers the bot is in'))
-    .addSubcommand((subcommand) => subcommand.setName('users').setDescription('see the users that installed the bot')),
+    .addSubcommand((subcommand) => subcommand.setName('users').setDescription('see the users that installed the bot'))
+    .addSubcommand((subcommand) => subcommand.setName('uptime').setDescription('view the bot uptime')),
   test: true,
   async execute(interaction) {
     if (interaction.options.getSubcommand() === 'servers') {
@@ -19,6 +20,9 @@ module.exports = {
     if (interaction.options.getSubcommand() === 'users') {
       const app = await interaction.client.application.fetch();
       return interaction.reply({ content: `The bot is installed by approximately \`${app.approximateUserInstallCount}\` users`, flags: MessageFlags.Ephemeral });
+    }
+    if (interaction.options.getSubcommand() === 'uptime') {
+      return interaction.reply({ content: `The bot has been up since ${time(interaction.client.readyAt, 'R')}`, flags: MessageFlags.Ephemeral });
     }
   },
 };
